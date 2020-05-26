ba4e85fe742d33e0fa541521806eadeefe2d191a
jwlee07 <apzk21413@gmail.com>
2020-05-26 18:36:55 +0900

    feat: tableView(numbers,refresh,section)

--- project/200525.project/TableViewPractice/Practice/TableViewSection.swift
+++ project/200525.project/TableViewPractice/Practice/TableViewSection.swift
@@ -63,20 +63,12 @@ final class TableViewSection: UIViewController {
   }
   
   func inPutDic() {
-    for index in 0...9 {
-      numberCategories.updateValue([index * 10], forKey: index)
-    }
     for index in data {
-      if 0 <= index, 10 > index {numberCategories[0]?.append(index)}
-      else if 10 < index, 20 > index {numberCategories[1]?.append(index)}
-      else if 20 < index, 30 > index {numberCategories[2]?.append(index)}
-      else if 30 < index, 40 > index {numberCategories[3]?.append(index)}
-      else if 40 < index, 50 > index {numberCategories[4]?.append(index)}
-      else if 50 < index, 60 > index {numberCategories[5]?.append(index)}
-      else if 60 < index, 70 > index {numberCategories[6]?.append(index)}
-      else if 70 < index, 80 > index {numberCategories[7]?.append(index)}
-      else if 80 < index, 90 > index {numberCategories[8]?.append(index)}
-      else if 90 < index, 100 > index {numberCategories[9]?.append(index)}
+      if let _ = numberCategories[ index / 10] {
+        numberCategories[ index / 10]?.append(index)
+      } else {
+        numberCategories[ index / 10] = [index]
+      }
     }
   }
   
