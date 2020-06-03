a3405c04cd1f26c4229bf3fcbf394e00b7b3e35e
jwlee07 <apzk21413@gmail.com>
2020-06-03 21:32:15 +0900

    feat: PhoneCall, MessageSenderProject

---
+++ project/200603.project/PhoneCallProject/MessageDATA.swift
@@ -0,0 +1,30 @@
+//
+//  MessageDATA.swift
+//  PhoneCallProject
+//
+//  Created by 이진욱 on 2020/06/03.
+//  Copyright © 2020 jwcheon. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct MessageData {
+  
+  static let sginInButton = UIButton()
+  static let sginOutButton = UIButton()
+  
+  static var groupTextfield = UITextField()
+  static var nameTextfield = UITextField()
+  static var numberTextfield = UITextField()
+  
+  static var group: [String] = ["그룹 리스트 확인","그룹 추가하기"]
+  static var name: [String] = [""]
+  static var number: [String] = []
+  static var messageText: String = ""
+  static var messageUserList: [String:[String:String]] = [:]
+  
+  static var messageDefault = UserDefaults.standard
+}
+
+
---
+++ project/200603.project/PhoneCallProject/UserInfoViewController.swift
@@ -0,0 +1,21 @@
+//
+//  UserInfoViewController.swift
+//  PhoneCallProject
+//
+//  Created by 이진욱 on 2020/06/03.
+//  Copyright © 2020 jwcheon. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      setupView()
+    }
+  func setupView() {
+    view.backgroundColor = .systemBackground
+    title = "그룹 리스트"
+  }
+}
